00c55b929324af9853114f53679900a68552324c
Guy Halperin & Michael Derfler <student@epicodus.com>
2014-07-23 16:21:45 -0700

    works on two sentences

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,18 +1,50 @@
 def calc(string)
-  array = string.split
-  if array[3] == "plus"
-    array[2].to_f + array[4].to_f
-  elsif array[3] == "minus"
-    array[2].to_f - array[4].to_f
-  elsif array[3] == "times"
-    array[2].to_f * array[4].to_f
-  elsif array[3] == "divided"
-    array[2].to_f / array[5].to_f
-  elsif array[6] == "power?"
-    array[5].slice!(-2..-1)
+  array = string.split("?")
+  final = []
 
-    array[2].to_f**array[5].to_f
+  finalarray = []
+  array1 = array[0].split
+  finalarray<<array1
+
+  if array.length > 1
+    array2 = array[1].split
+    finalarray<<array2
+  end
+
+  finalarray.each do |x|
+    if x[3] == "plus"
+      result = x[2].to_f + x[4].to_f
+      final<<result
+    elsif x[3] == "minus"
+      result = x[2].to_f - x[4].to_f
+      final<<result
+    elsif x[3] == "times"
+      result = x[2].to_f * x[4].to_f
+      final<<result
+    elsif x[3] == "divided"
+      result = x[2].to_f / x[5].to_f
+      final<<result
+    elsif x[6] == "power"
+      x[5].slice!(-2..-1)
+      result = x[2].to_f**x[5].to_f
+      final<<result
+    end
+
+    if x[5] == "plus"
+      final[0] = final[0] + x[6].to_f
+    elsif x[5] == "minus"
+      final[0] = final[0] - x[6].to_f
+    elsif x[5] == "times"
+      final[0] = final[0] * x[6].to_f
+    elsif x[5] == "divided"
+      final[0] = final[0] / x[7].to_f
+    else
+      final
+    end
 
   end
+  final
+
 end
-puts calc("what is 4 to the 3rd power?")
+
+ puts calc("what is 4 plus 8 plus 6?")
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,18 +4,24 @@ require ('calc.rb')
 describe("calculates math problems in words") do
 
   it("does addition") do
-    calc("What is 1 plus 1?").should(eq(2))
+    calc("What is 1 plus 1?").should(eq([2]))
   end
   it("does subtraction") do
-    calc("What is 3 minus 1?").should(eq(2))
+    calc("What is 3 minus 1?").should(eq([2]))
   end
   it("does multiplication") do
-    calc("What is 3 times 3?").should(eq(9))
+    calc("What is 3 times 3?").should(eq([9]))
   end
   it("does division") do
-    calc("What is 7 divided by 2?").should(eq(3.5))
+    calc("What is 7 divided by 2?").should(eq([3.5]))
   end
-  it("handles exponents")do
-    calc("what is 4 to the 3rd power?").should(eq(64))
+  it("handles exponents") do
+    calc("what is 4 to the 3rd power?").should(eq([64]))
+  end
+  it("handles multiple operations") do
+    calc("what is 4 plus 8 times 3?").should(eq([36]))
+  end
+  it("handles multiple sentences") do
+    calc("what is 4 times 3? what is 4 times 2?").should(eq([12,8]))
   end
 end
